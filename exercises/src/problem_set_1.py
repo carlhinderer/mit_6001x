6ce7a0ac7a35b665a4ae3eee94e2add930c609da
Carl Hinderer <carl.hinderer4@gmail.com>
2018-10-30 16:21:02 -0600

    Did Ps1 problem 2.

--- exercises/src/problem_set_1.py
+++ exercises/src/problem_set_1.py
@@ -19,3 +19,35 @@ def count_vowels(s):
 def test_count_vowels_interactively():
     s = input('Enter a string: ')
     print('Number of vowels:', count_vowels(s))
+
+
+# Problem 2
+# Assume s is a string of lower case characters.
+#
+# Write a program that prints the number of times the string 'bob' occurs in s. For example, if
+#   s = 'azcbobobegghakl', then your program should print:
+# Number of times bob occurs is: 2
+
+
+def count_bobs(s):
+    bob_count = 0
+    for i in range(len(s) - 2):
+        if s[i:i + 3] == 'bob':
+            bob_count += 1
+    return bob_count
+
+
+def test_count_bobs_interactively():
+    s = input('Enter a string: ')
+    print('Number of times bob occurs is:', count_bobs(s))
+
+
+# Problem 3
+# Assume s is a string of lower case characters.
+#
+# Write a program that prints the longest substring of s in which the letters occur in alphabetical order.
+# For example, if s = 'azcbobobegghakl', then your program should print:
+# Longest substring in alphabetical order is: beggh
+#
+# In the case of ties, print the first substring. For example, if s = 'abcbcd', then your program should print:
+# Longest substring in alphabetical order is: abc
--- exercises/tests/problem_set_1_test.py
+++ exercises/tests/problem_set_1_test.py
@@ -2,8 +2,15 @@ import pytest
 import src.problem_set_1 as ps1
 
 
-def test_vowel_count():
+def test_count_vowels():
     assert ps1.count_vowels('') == 0
     assert ps1.count_vowels('bcd') == 0
     assert ps1.count_vowels('abc') == 1
     assert ps1.count_vowels('abcdefghijklm') == 3
+
+
+def test_count_bobs():
+    assert ps1.count_bobs('') == 0
+    assert ps1.count_bobs('abcdefg') == 0
+    assert ps1.count_bobs('abcbobdef') == 1
+    assert ps1.count_bobs('bobob') == 2
