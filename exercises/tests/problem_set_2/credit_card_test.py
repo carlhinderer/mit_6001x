136e79c472b239033b6cc7eacb60764d0286fc2a
Carl Hinderer <carl.hinderer4@gmail.com>
2018-11-08 21:40:48 -0700

    Added tests for ps2 exercise 1.

--- exercises/tests/problem_set_2/credit_card_test.py
+++ exercises/tests/problem_set_2/credit_card_test.py
@@ -2,8 +2,13 @@ import pytest
 from src.problem_set_2 import credit_card
 
 
-def test_make_years_minimum_payments():
+def test_bal_after_years_min_pmts():
     balance = 42
     interest_rate = 0.2
     pmt_rate = 0.04
-    assert credit_card.make_years_minimum_payments(balance, interest_rate, pmt_rate) == 'test'
+    assert credit_card.bal_after_years_min_pmts(balance, interest_rate, pmt_rate) == 31.38
+
+    balance = 484
+    interest_rate = 0.2
+    pmt_rate = 0.04
+    assert credit_card.bal_after_years_min_pmts(balance, interest_rate, pmt_rate) == 361.61
