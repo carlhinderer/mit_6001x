86f8e2be670f04adfe4a98f27b52fad2c48bb16a
Carl Hinderer <carl.hinderer4@gmail.com>
2018-11-09 11:46:54 -0700

    Added tests for ps2 exercise 2.

--- exercises/src/problem_set_2/interactive_tester.py
+++ exercises/src/problem_set_2/interactive_tester.py
@@ -1,9 +1,16 @@
 from . import credit_card
 
 
-def test_bal_after_years_min_pmts_interactively():
+def test_bal_after_years_min_pmts():
     balance = float(input('Enter the starting balance: '))
     annual_int_rate = float(input('Enter the annual interest rate: '))
     monthly_pmt_rate = float(input('Enter the monthly_pmt_rate: '))
     remaining_bal = credit_card.bal_after_years_min_pmts(balance, annual_int_rate, monthly_pmt_rate)
     print('Remaining balance:', remaining_bal)
+
+
+def test_pmt_to_pay_bal_in_year():
+    balance = float(input('Enter the starting balance: '))
+    annual_int_rate = float(input('Enter the annual interest rate: '))
+    minimum_pmt = credit_card.pmt_to_pay_bal_in_year(balance, annual_int_rate)
+    print('Lowest payment:', minimum_pmt)
--- exercises/tests/problem_set_2/credit_card_test.py
+++ exercises/tests/problem_set_2/credit_card_test.py
@@ -12,3 +12,17 @@ def test_bal_after_years_min_pmts():
     interest_rate = 0.2
     pmt_rate = 0.04
     assert credit_card.bal_after_years_min_pmts(balance, interest_rate, pmt_rate) == 361.61
+
+
+def test_pmt_to_pay_bal_in_year():
+    balance = 3329
+    interest_rate = 0.2
+    assert credit_card.pmt_to_pay_bal_in_year(balance, interest_rate) == 310
+
+    balance = 4773
+    interest_rate = 0.2
+    assert credit_card.pmt_to_pay_bal_in_year(balance, interest_rate) == 440
+
+    balance = 3926
+    interest_rate = 0.2
+    assert credit_card.pmt_to_pay_bal_in_year(balance, interest_rate) == 360
