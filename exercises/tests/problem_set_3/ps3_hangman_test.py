c4126c123083c0419b6d49bf96800e7f994aa1f1
Carl Hinderer <carl.hinderer4@gmail.com>
2018-11-12 22:55:00 -0700

    Added tests for ps3 exercise 1.

---
+++ exercises/tests/problem_set_3/ps3_hangman_test.py
@@ -0,0 +1,14 @@
+import pytest
+from src.problem_set_3 import ps3_hangman
+
+
+def test_isWordGuessed():
+    word = 'testword'
+    letters_guessed = ['t', 'e', 's', 'w', 'o', 'r', 'd']
+    assert ps3_hangman.isWordGuessed(word, letters_guessed) == True
+
+    letters_guessed = ['t', 'e', 's']
+    assert ps3_hangman.isWordGuessed(word, letters_guessed) == False
+
+    letters_guessed = []
+    assert ps3_hangman.isWordGuessed(word, letters_guessed) == False
